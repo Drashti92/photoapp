198f960deab835c8febd983632f7379f3297ef25
drashtiblimbachiya <drashtil92@gmail.com>
2017-02-13 19:57:07 +0000

    Implemented stripe api

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
 Rails.configuration.stripe = {
 
-:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+    :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
 
-:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+    :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = "sk_test_whfks74oYAFCwl8Maf9Y75O8"
